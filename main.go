930a4c70d9cc213cb8843759d7a74f273feb0993
uu4k <t.shimizu1115@gmail.com>
2018-04-29 10:43:39 +0900

    update packages.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"homecast"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -15,6 +14,7 @@ import (
 
 	"github.com/urfave/cli"
 
+	"github.com/uu4k/homecast"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
